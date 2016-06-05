6b6eb36db0c0201beba275b662b2704d24db32ef
Ben Sparks <bsparks42@gmail.com>
2016-06-04 20:31:29 -0700

    load the main image

--- game/states/load.js
+++ game/states/load.js
@@ -3,6 +3,8 @@ import {Phaser} from 'phaser';
 export default class LoadState extends Phaser.State {
     preload() {
         this.game.add.text(this.game.world.bounds.centerX, this.game.world.bounds.centerY, 'loading...', {fill: '#fff'});
+        
+        this.game.load.image('tileset', 'assets/tileset.png');
     }
 
     create() {
